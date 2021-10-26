b72b787796a668126460958e265fbfcfad4a1233
PunyaSaya19 <rifanhdy91@gmail.com>
2021-10-26 13:23:47 +0700

    fixed bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,135 +3,136 @@ const my_navbar = document.getElementById("my-navbar");
 const cek_artikel = window.localStorage.getItem("articel");
 const row_blog_content = document.getElementById("row-blog-content");
 const recent_post = document.getElementById("recent-post");
+const search_input = document.getElementById('search-field');
+const blog_banner = document.getElementById("blog-banner");
+document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+alert('ok')
+
+
 // action
 window.onscroll = () => {
-  if(window.pageYOffset > 0){
+  if (window.pageYOffset > 0) {
     my_navbar.classList.add("bg-navbar");
-  }else{
+  } else {
     my_navbar.classList.remove("bg-navbar");
   }
 }
 window.addEventListener("DOMContentLoaded", () => {
-  if(cek_artikel == null){
+  if (cek_artikel == null) {
     createFirstArticel();
   }
   showAllArticel();
   showRecentPost();
 })
 
+
 // functions
-function randomId()
-{
+function randomId() {
   let r = Math.random().toString(36).substring(8);
   return r;
 }
-function getDateNow()
-{
+function getDateNow() {
   const date = new Date();
   const Y = date.getFullYear();
   const m = date.getMonth() + 1;
   let d = date.getDate();
-  if(d < 10){
+  if (d < 10) {
     d = "0" + d;
   }
   return `${Y}-${m}-${d}`;
 }
-function createFirstArticel() 
-{
+function createFirstArticel() {
   const data = [
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 2",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 2",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 3",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 3",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 4",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 4",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 5",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 5",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 6",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 6",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 7",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 7",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 8",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 8",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
     {
-    id : randomId(),
-    title : "Welcome To PunyaSaya Blogs 9",
-    category : "other",
-    created_at : getDateNow(),
-    image : "other.jpg",
-    content : "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
-     },
-     ];
+      id: randomId(),
+      title: "Welcome To PunyaSaya Blogs 9",
+      category: "other",
+      created_at: getDateNow(),
+      image: "other.jpg",
+      content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ducimus vero, voluptate iusto quod qui, aspernatur distinctio, praesentium totam ea quaerat laborum eum facere at!</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque alias repellat dolorem consectetur corrupti. Temporibus incidunt reiciendis, sed, quidem ut natus excepturi numquam facilis beatae maiores minus autem nulla explicabo.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, ad!</p>"
+    },
+  ];
   window.localStorage.setItem("articel", JSON.stringify(data));
 }
-function showAllArticel()
-{
+function showAllArticel() {
   let articel = JSON.parse(window.localStorage.getItem("articel"));
   row_blog_content.innerHTML = "";
   row_blog_content.innerHTML = myTemplate(articel);
 }
-function convertDate(data)
-{
-   const arr = data.split("-");
-   const arrBlnIndo = [
-      "Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"
-     ];
-   let bln = parseInt(arr[1]) - 1;
-   bln = arrBlnIndo[bln];
-   return `${arr[2]} ${bln} ${arr[0]}`;
+function convertDate(data) {
+  const arr = data.split("-");
+  const arrBlnIndo = [
+    "Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+  ];
+  let bln = parseInt(arr[1]) - 1;
+  bln = arrBlnIndo[bln];
+  return `${arr[2]} ${bln} ${arr[0]}`;
 }
-function myTemplate(data) 
-{
+function myTemplate(data) {
   let tmplt = "";
   data.forEach((d) => {
     let content = d.content.substr(3, 75);
@@ -141,8 +142,11 @@ function myTemplate(data)
               <div class="col-12 col-md-4 col-lg-4">
                 <article class="article article-style-b">
                   <div class="article-header">
+                    <!--
                     <div class="article-image" data-background="assets/img/category/${d.image}">
                     </div>
+                    -->
+                    <img src="assets/img/category/${d.image}" class="w-100">
                   </div>
                   <div class="article-details">
                     <div class="article-title">
@@ -173,24 +177,21 @@ function myTemplate(data)
   });
   return tmplt;
 }
-function detailArticel(e)
-{
+function detailArticel(e) {
   const id = e.getAttribute("data-id");
   window.localStorage.setItem("id-detail-articel", id);
   window.location.href = "detail.html";
 }
-function showRecentPost()
-{
+function showRecentPost() {
   let articel = JSON.parse(window.localStorage.getItem("articel"));
   articel = articel.reverse();
   recent_post.innerHTML = "";
   recent_post.innerHTML = recentTemplate(articel);
 }
-function recentTemplate(data)
-{
+function recentTemplate(data) {
   let tmplt = ``;
   data.forEach((e, i) => {
-    if(i < 5){
+    if (i < 5) {
       tmplt += `
           <li>
             <a href="#" onclick="detailArticel(this)" data-id="${e.id}">
@@ -201,4 +202,30 @@ function recentTemplate(data)
     }
   })
   return tmplt;
+}
+function searchBlogs() {
+  const search_query = search_input.value.toLowerCase();
+  const all_articel = JSON.parse(window.localStorage.getItem('articel'));
+  let articels = [];
+  all_articel.forEach((e, i) => {
+    let articel_title = e.title.toLowerCase();
+    //console.log(articel_title.includes(search_query))
+    if (articel_title.includes(search_query) == true) {
+      articels.push(e);
+    }
+  });
+  console.log(articels == null);
+  row_blog_content.innerHTML = "";
+  if (articels.length < 1) {
+    row_blog_content.innerHTML = `
+    <div class="col">
+              <p class="text-center text-danger">
+                Artikel Tidak Ditemukan!!
+              </p>
+            </div>
+    `
+  } else {
+    row_blog_content.innerHTML = myTemplate(articels);
+  }
+  window.scrollTo(0, blog_banner.clientHeight);
 }
