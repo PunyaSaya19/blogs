a04b6ba14780d86eb49f90e858fc8ef81db4495a
PunyaSaya19 <rifanhdy91@gmail.com>
2021-10-30 12:59:26 +0700

    create login

---
+++ assets/js/login.js
@@ -0,0 +1,34 @@
+// email and password login
+const email = 'punyasaya@official.com';
+const pass = 'punyasaya';
+
+// variabels 
+const iEmail = document.getElementById('email');
+const iPass = document.getElementById('password');
+const btnSubmit = document.getElementById('btn-submit');
+const loginFeedback = document.getElementById('login-feedback');
+
+// action 
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (iEmail.value == "" || iPass.value == "") {
+        loginFeedback.innerHTML = "";
+        loginFeedback.innerHTML = `
+             <div class="alert alert-warning" role="alert">
+                Harap Untuk Mengisi Email dan Password!!
+             </div>
+        `;
+        return false;
+    }
+    if (iEmail.value != email || iPass.value != pass) {
+        loginFeedback.innerHTML = "";
+        loginFeedback.innerHTML = `
+             <div class="alert alert-danger" role="alert">
+                Oupsss.. Email atau Password SALAH!!!
+             </div>
+        `;
+        return false;
+    }
+    window.localStorage.setItem('login', 'true');
+    window.location.href = "admin/dashboard.html";
+})
